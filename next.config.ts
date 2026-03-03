5a3c18bae6d284fa4a3263b65a800f8e42d688dc
Luannt1005 <100223397+Luannt1005@users.noreply.github.com>
2026-03-03 14:48:37 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,25 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            // Cho phép nhúng iframe từ mọi nguồn (X-Frame-Options)
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            // Content Security Policy cho phép nhúng (frame-ancestors *)
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *",
+          },
+        ],
+      },
+    ]
+  },
 };
-
 
 export default nextConfig;
